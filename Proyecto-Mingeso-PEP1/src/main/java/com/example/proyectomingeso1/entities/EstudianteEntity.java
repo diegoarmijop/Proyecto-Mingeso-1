e1b4fa498dce73518bd17ba923cf4b165fa2f1e2
Diego Armijo <diego.armijo.p@usach.cl>
2023-09-28 19:57:32 -0300

    Se inicio el proyecto y se creo la entidad Estudiante.

---
+++ Proyecto-Mingeso-PEP1/src/main/java/com/example/proyectomingeso1/entities/EstudianteEntity.java
@@ -0,0 +1,28 @@
+package com.example.proyectomingeso1.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name="estudiantes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EstudianteEntity {
+
+    @Id
+    private String RUT;
+    private String apellidos;
+    private String nombres;
+    private Date fechaNacimiento;
+    private String tipoColegioProcedencia;
+    private String nombreColegio;
+    private Integer anoEgresoColegio;
+
+}
